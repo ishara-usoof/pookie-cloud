54eb2e224bc9cfaf6814713d05dfa06d478f7823
Ishara Usoof <ishara.20220380@iit.ac.lk>
2024-04-29 22:30:06 +0530

    changed to remove violation

--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/customer", proxy("http://app-lb-1120431137.eu-north-1.elb.amazonaws.com"));
-app.use("/shopping", proxy("http://app-lb-1120431137.eu-north-1.elb.amazonaws.com"));
-app.use("/products", proxy("http://app-lb-1120431137.eu-north-1.elb.amazonaws.com")); 
+app.use("/customer", proxy("http://....elb.amazonaws.com"));
+app.use("/shopping", proxy("http://....elb.amazonaws.com"));
+app.use("/products", proxy("http://....elb.amazonaws.com")); 
 
 app.listen(80, () => {
   console.log("Gateway is Listening to Port 80");
